0f56d90af5ca9a8efc58daf0de56d393bf5ba37a
Loren Schwartz <lorencanbefound@gmail.com>
2018-03-25 16:56:40 -0400

    resolved remaining conflicts

--- init.el
+++ init.el
@@ -1,6 +1,4 @@
 (setq debug-on-error t)
-<<<<<<< HEAD
-
 
 (require 'package)
 (add-to-list 'package-archives
@@ -13,14 +11,13 @@
   (package-install 'use-package))
 
 (setq shell-command-switch "-ic")
-=======
+
 (package-initialize)
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (require 'use-package)
 (exec-path-from-shell-copy-env "PATH")
->>>>>>> 33cc63a7899d1f164d59ec60f1d547c85e1d347e
 
 (require 'org)
 (org-babel-load-file "~/.emacs.d/configuration.org")
@@ -32,7 +29,5 @@
 (setq custom-file
       (expand-file-name "custom.el" user-emacs-directory))
 (load-file custom-file)
-<<<<<<< HEAD
+
 (exec-path-from-shell-initialize)
-=======
->>>>>>> 33cc63a7899d1f164d59ec60f1d547c85e1d347e
